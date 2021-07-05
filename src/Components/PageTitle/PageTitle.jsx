9582bc01eb863378de33060b4345ea4facd1d744
Dulya Perera <57215584+Dulyaaa@users.noreply.github.com>
2021-07-05 14:33:53 +0530

    Add files via upload

---
+++ src/Components/PageTitle/PageTitle.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { Card } from 'react-bootstrap';
+import logo from 'url:~/src/Assets/title.jpg';
+
+export default class PageTitle extends Component {
+
+    render() {
+        return (
+            <div>
+                <Card className="bg-dark text-white">
+                    <Card.Img src={logo} alt="Card image" style={{ height: 170 }} />
+                    <Card.ImgOverlay>
+                        <Card.Text style={{ fontSize: 50, paddingTop: 30, textAlign: 'center', fontWeight: "bolder" }}>{this.props.title}</Card.Text>
+                    </Card.ImgOverlay>
+                </Card>
+            </div>
+        );
+    }
+}
