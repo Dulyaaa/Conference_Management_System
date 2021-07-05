5e88bfd4382e3c89fe940b0081c43451c0942c99
Dulya Perera <57215584+Dulyaaa@users.noreply.github.com>
2021-07-05 14:17:35 +0530

    Update http-common.jsx

--- src/http-common.jsx
+++ src/http-common.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://icaf-cms-backend.herokuapp.com",
   headers: {
     "Content-type": "application/json"
   }
