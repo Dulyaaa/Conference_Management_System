4641dcba366744cb1edfbb60a0181219e3d1012e
Dulya Perera <57215584+Dulyaaa@users.noreply.github.com>
2021-07-05 14:33:40 +0530

    Add files via upload

---
+++ src/Components/Logout/Logout.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+
+export default class Logout extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+                 
+        }
+
+        this.handleEvent = this.handleEvent.bind(this)
+    }
+
+    componentDidMount() {
+        
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) { if (prevState.name !== this.state.name) { this.handler() } }
+
+    componentWillUnmount() {
+        
+    }
+
+    // Prototype methods, Bind in Constructor (ES2015)
+    handleEvent() {}
+
+    // Class Properties (Stage 3 Proposal)
+    handler = () => { this.setState() }
+
+    render() {
+        window.sessionStorage.removeItem("UserRole") 
+        return (
+            <>
+                
+            </>
+        )
+    }
+}
