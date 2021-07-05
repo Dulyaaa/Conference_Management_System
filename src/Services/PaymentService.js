79600fd3d64cf191c35a37d12c444ee3e6411ba6
Dulya Perera <57215584+Dulyaaa@users.noreply.github.com>
2021-07-05 14:18:01 +0530

    Update PaymentService.js

--- src/Services/PaymentService.js
+++ src/Services/PaymentService.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 class PaymentService{
 
     addpayment(payment){
-        return axios.post('http://localhost:8080/payments/addPayment',payment)
+        return axios.post('https://icaf-cms-backend.herokuapp.com/payments/addPayment',payment)
     }
 
 }
