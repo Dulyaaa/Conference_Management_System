f1d5753c275d685f3e88f50b857f80f620ed2d22
Dulya Perera <57215584+Dulyaaa@users.noreply.github.com>
2021-07-05 14:18:33 +0530

    Update RegisterUserService.jsx

--- src/Services/RegisterUserService.jsx
+++ src/Services/RegisterUserService.jsx
@@ -2,19 +2,19 @@ import axios from 'axios';
 
  class RegisterUserService{
     addCustomer(user){
-        return axios.post('http://localhost:8080/users/addUser',user)
+        return axios.post('https://icaf-cms-backend.herokuapp.com/users/addUser',user)
     }
 
     getUserByIdOptional(cEmail){
-        return axios.get('http://localhost:8080/users/opUsers'+ '/' +cEmail);
+        return axios.get('https://icaf-cms-backend.herokuapp.com/users/opUsers'+ '/' +cEmail);
     }
 
     updateuser(email ,user){
-        return  axios.put('http://localhost:8080/users/updateUser'+ '/' +email , user)
+        return  axios.put('https://icaf-cms-backend.herokuapp.com/users/updateUser'+ '/' +email , user)
     }
 
     deleteUser(email){
-        return axios.delete('http://localhost:8080/users/delete'+ '/' +email)
+        return axios.delete('https://icaf-cms-backend.herokuapp.com/users/delete'+ '/' +email)
     }
 
 
